ae658114cd478e1a6730dd68b9d478ab5fc600a9
Alexander I.Grafov <grafov@inet.name>
2025-03-13 17:25:07 +0300

    refactor(scripts): clear selection when it obsoleted
    Because selection invalidated by move&remove.

--- fargs-funcs.fish
+++ fargs-funcs.fish
@@ -1,21 +1,23 @@
 # Like a "cp" command, but for selected by "fargs" files.
 function cps
-    for f in (fsel o)
+    for f in (fsel out)
         cp -a $f $argv
     end
 end
 
 # Like a "mv" command, but for selected by "fargs" files.
 function mvs
-    for f in (fsel o)
+    for f in (fsel out)
         mv $f $argv
     end
+    fsel clean
 end
 
 # Like a "rm" command, but for selected by "fargs" files.
 # Dangerous! By default uses option for recursive removal.
 function rms
-    for f in (fsel o)
+    for f in (fsel out)
         rm -rf $f $argv
     end
+    fsel clean
 end
--- rms.sh
+++ rms.sh
@@ -2,3 +2,4 @@
 # Like a "rm" command, but for selected by "fargs" files.
 # Dangerous! By default uses option for recursive removal.
 for f in $(fsel o); do rm -rf $f $argv; done
+fsel c
